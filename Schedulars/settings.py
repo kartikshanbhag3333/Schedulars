1b8e3b68a43dec459a33d7ecb99f7b57acf85574
kartikshanbhag3333 <kartikshanbhag3333@gmail.com>
2015-03-18 11:58:33 +0530

    Update settings.py

--- Schedulars/settings.py
+++ Schedulars/settings.py
@@ -70,13 +70,9 @@ APPEND_SLASH=True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mywork',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 # Internationalization
